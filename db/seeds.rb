2052337860e38effe80ba140975ac7ffda13a914
Jeferson Alves <jeferson.alves@rdstation.com>
2022-12-17 13:10:10 -0300

    refactor(Users): :construction_worker: change default id to custom uuid

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.delete_all
 
 7.times do
   User.create(
-    id: Faker::Internet.uuid,
+    uuid: Faker::Internet.uuid,
     email: Faker::Internet.email,
     password: Faker::Internet.password,
     nickname: Faker::Name.name,
--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
-  #   cookies.signed[:user_id] = 42
+  #   cookies.signed[:user_uuid] = 'a0a9df68-7e23-11ed-a1eb-0242ac120002'
   #
   #   connect
   #
-  #   assert_equal connection.user_id, "42"
+  #   assert_equal connection.user_id, "a0a9df68-7e23-11ed-a1eb-0242ac120002"
   # end
 end
