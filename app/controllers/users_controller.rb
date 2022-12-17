7bbb17e93e2d04019a814d389e7d8c1ad225b206
Jeferson Alves <jeferson.alves@rdstation.com>
2022-12-17 11:11:43 -0300

    feat(Users): :sparkles: simple settings of user api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    render json: { message: 'Hello from UsersController' }
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render json: @user
+  end
 end
