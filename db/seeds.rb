6bffa867f156b045a17d2543707ae0cfc6991bb0
Jeferson Alves <jeferson.alves@rdstation.com>
2023-01-03 15:05:48 +0000

    revert(Users): :fire: remove user, let's restart!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
 require 'faker'
-
-User.delete_all
-
-7.times do
-  User.create(
-    uuid: Faker::Internet.uuid,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,
-    nickname: Faker::Name.name,
-  )
-end
--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,11 +1,4 @@
 require "test_helper"
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
-  #   cookies.signed[:user_uuid] = 'a0a9df68-7e23-11ed-a1eb-0242ac120002'
-  #
-  #   connect
-  #
-  #   assert_equal connection.user_id, "a0a9df68-7e23-11ed-a1eb-0242ac120002"
-  # end
 end
